cf320a58193cb6893c89ce8834461ff6b9a22230
Asulan Maksut <a.maksut@strongte.am>
2024-04-03 14:46:49 +0500

    provide description of endpoints of AuthController

--- src/main/java/com/halyk/onlinestore/controller/AuthController.java
+++ src/main/java/com/halyk/onlinestore/controller/AuthController.java
@@ -1,9 +1,18 @@
 package com.halyk.onlinestore.controller;
 
+import com.halyk.onlinestore.dto.ErrorResponse;
 import com.halyk.onlinestore.dto.auth.request.AuthRequest;
 import com.halyk.onlinestore.dto.auth.request.RefreshRequest;
 import com.halyk.onlinestore.dto.auth.response.AuthResponse;
 import com.halyk.onlinestore.service.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -15,15 +24,68 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/auth")
+@Tag(name = "Authorization", description = "Authorization and token refresh")
 public class AuthController {
 
     private final AuthService authService;
 
+    @Operation(summary = "User login")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "In case of successful authorization, pair of access and refresh tokens are returned",
+                    content = @Content(schema = @Schema(implementation = AuthResponse.class))
+            ),
+            @ApiResponse(responseCode = "400",
+                    description = """
+                            If fields 'username' or 'password' are blank, then corresponding response
+                            with invalid field messages is returned
+                            """,
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class))
+            ),
+
+            @ApiResponse(responseCode = "401",
+                    description = """
+                            If an incorrect username or an incorrect password is sent, the service returns
+                            a general authorization error with `status = 401` and the following description of the reason in
+                            the `message` field:
+                                          
+                              `Unauthorized`
+                            """,
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class))
+            ),
+    })
     @PostMapping
     public ResponseEntity<AuthResponse> login(@RequestBody @Valid AuthRequest request) {
         return ResponseEntity.ok(authService.login(request));
     }
 
+    @Operation(summary = "Refresh tokens")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "In case of successful refresh, pair of access and refresh tokens are returned",
+                    content = @Content(schema = @Schema(implementation = AuthResponse.class))
+            ),
+            @ApiResponse(responseCode = "400",
+                    description = """
+                            If field 'refreshToken' is blank, then corresponding response
+                            with invalid field messages is returned
+                            """,
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class))
+            ),
+
+            @ApiResponse(responseCode = "401",
+                    description = """
+                            If an invalid refreshToken is sent, the service returns
+                            a general authorization error with `status = 401` and the following description of the reason in
+                            the `message` field:
+                                          
+                              `Unauthorized`
+                            """,
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class))
+            ),
+    })
     @PostMapping("/refresh")
     public ResponseEntity<AuthResponse> refresh(@RequestBody @Valid RefreshRequest request) {
         return ResponseEntity.ok(authService.refresh(request));
